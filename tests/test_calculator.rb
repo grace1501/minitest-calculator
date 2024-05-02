660395d6e3ec2bb7feb3ce8208a1edf46fbf497c
Grace Do <61570746+grace1501@users.noreply.github.com>
2024-05-02 20:20:17 +0000

    all test completed

---
+++ tests/test_calculator.rb
@@ -0,0 +1,15 @@
+require 'minitest/autorun'
+require './calculator'
+
+class TestCalculator < Minitest::Test
+  def test_addition
+    calculator = Calculator.new
+    assert_equal 4, calculator.add(2,2), "Addition method failed"
+  end
+
+  def test_subtraction
+    calculator = Calculator.new
+    assert_equal 4, calculator.subtract(8,4), "Subtraction method failed"
+  end
+  
+end
